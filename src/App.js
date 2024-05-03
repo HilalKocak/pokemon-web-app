a909b981a6266f9762828ba9eb1018d6322ee21f
Hilal38 <hhilalkocak@gmail.com>
2024-05-03 12:34:17 +0300

    Fetch pokemons from api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, {useState} from 'react'
 import PokemonList from './PokemonList';
-
+import axios from 'axios';
 function App() {
   const [pokemon, setPokemon] = useState(["bulbasaur", "charmandar"])
+  axios.get("https://pokeapi.co/api/v2/pokemon").then(res => {
+    setPokemon(res.data.results.map(p => p.name))
+  })
   return (
     <PokemonList pokemon={pokemon} />
   );
