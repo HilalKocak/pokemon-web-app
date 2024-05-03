e803b9a41a708bb9d0af0a89000290c9a8652b37
Hilal38 <hhilalkocak@gmail.com>
2024-05-03 13:03:57 +0300

    Manage errors with axious

--- src/App.js
+++ src/App.js
@@ -1,13 +1,59 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
 import PokemonList from './PokemonList';
 import axios from 'axios';
+import Pagination from './Pagination';
 function App() {
+  
   const [pokemon, setPokemon] = useState(["bulbasaur", "charmandar"])
-  axios.get("https://pokeapi.co/api/v2/pokemon").then(res => {
-    setPokemon(res.data.results.map(p => p.name))
-  })
+  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [nextPageUrl, setNextPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [prevPageUrl, setPrevPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setLoading(true)
+    let cancel
+    axios.get(currentPageUrl, {
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    }).
+    then(res => {
+      setLoading(false)
+      setNextPageUrl(res.data.next)
+      setPrevPageUrl(res.data.previous)
+      setPokemon(res.data.results.map(p => p.name))
+    }).catch(e => {
+      if (axios.isCancel(e)) {
+        console.log('Request canceled', e.message);  
+      } else {
+        setLoading(false); 
+        console.error('An error occurred', e);
+      }
+    });
+
+    return () => {
+      cancel && cancel(); 
+    }
+
+  }, [currentPageUrl])
+  
+  function gotoNextPage() {
+    setCurrentPageUrl(nextPageUrl)
+  }
+
+  function gotoPrevPage() {
+    setCurrentPageUrl(prevPageUrl)
+  }
+  if(loading) return "Loading.."
+ 
   return (
+   <>
     <PokemonList pokemon={pokemon} />
+    <Pagination>
+      gotoNextPage = {nextPageUrl ? gotoNextPage : null}
+      gotoPrevPage = {prevPageUrl ? gotoPrevPage : null}
+    </Pagination>
+    
+    </>
   );
 }
 
