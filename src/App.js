4c9a89898e55829f89c39b6d9911366b10f2e0f0
Hilal38 <hhilalkocak@gmail.com>
2024-05-03 13:23:41 +0300

    Fix bug of passing props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react'
-import PokemonList from './PokemonList';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import PokemonList from './PokemonList'
+import axios from 'axios'
 import Pagination from './Pagination';
+
 function App() {
-  
-  const [pokemon, setPokemon] = useState(["bulbasaur", "charmandar"])
+  const [pokemon, setPokemon] = useState([])
   const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
-  const [nextPageUrl, setNextPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
-  const [prevPageUrl, setPrevPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [nextPageUrl, setNextPageUrl] = useState()
+  const [prevPageUrl, setPrevPageUrl] = useState()
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
@@ -15,27 +15,16 @@ function App() {
     let cancel
     axios.get(currentPageUrl, {
       cancelToken: new axios.CancelToken(c => cancel = c)
-    }).
-    then(res => {
+    }).then(res => {
       setLoading(false)
       setNextPageUrl(res.data.next)
       setPrevPageUrl(res.data.previous)
       setPokemon(res.data.results.map(p => p.name))
-    }).catch(e => {
-      if (axios.isCancel(e)) {
-        console.log('Request canceled', e.message);  
-      } else {
-        setLoading(false); 
-        console.error('An error occurred', e);
-      }
-    });
+    })
 
-    return () => {
-      cancel && cancel(); 
-    }
-
+    return () => cancel()
   }, [currentPageUrl])
-  
+
   function gotoNextPage() {
     setCurrentPageUrl(nextPageUrl)
   }
@@ -43,18 +32,18 @@ function App() {
   function gotoPrevPage() {
     setCurrentPageUrl(prevPageUrl)
   }
-  if(loading) return "Loading.."
- 
+
+  if (loading) return "Loading..."
+  
   return (
-   <>
-    <PokemonList pokemon={pokemon} />
-    <Pagination>
-      gotoNextPage = {nextPageUrl ? gotoNextPage : null}
-      gotoPrevPage = {prevPageUrl ? gotoPrevPage : null}
-    </Pagination>
-    
+    <>
+      <PokemonList pokemon={pokemon} />
+      <Pagination
+        gotoNextPage={nextPageUrl ? gotoNextPage : null}
+        gotoPrevPage={prevPageUrl ? gotoPrevPage : null}
+      />
     </>
   );
 }
 
-export default App; 
+export default App;
